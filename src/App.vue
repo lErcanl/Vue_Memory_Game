2b39238187595bb814825e1d4d8a4908a19bd3e2
Ercan Can <38925279+lErcanl@users.noreply.github.com>
2020-09-12 20:49:38 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+<transition-group name="slideContainer" mode="out-in">
+        <component key="maincomponenti" @activeComponentEvent="activeComponent=$event"
+        :is="activeComponent">
+            </component>
+    <canvas key=canvas id="canvas" v-show="activeComponent=='app-celebrate'"></canvas>
+
+
+</transition-group>
+
+</template>
+<script>
+import GameCards from "./components/GameCards";
+import Failure from "./components/Failure";
+import Celebrate from "./components/Celebrate";
+
+export default {
+    components:{
+        appGameCards:GameCards,
+        appFailure:Failure,
+        appCelebrate:Celebrate
+    },
+    data(){
+        return{
+            activeComponent:"app-game-cards"
+        }
+    }
+}
+</script>
+<style>
+.slideContainer-enter{
+
+}
+.slideContainer-enter-active{
+    animation:slide-in .3s ease-out forwards;
+
+}
+.slideContainer-leave{
+
+}
+.slideContainer-leave-active{
+    animation:slide-out .3s ease-out forwards;
+
+}
+@keyframes slide-in{
+    from{
+            transform:translateX(-1000px);
+    }
+    to{
+            transform:translateX(0);
+
+    }
+}
+@keyframes slide-out{
+    from{
+            transform:translateX(0);
+    }
+    to{
+            transform:translateX(1000px);
+
+    }
+}
+</style>
